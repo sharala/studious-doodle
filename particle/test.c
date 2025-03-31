e8454be60fa2ac41b8044fe8db7c8f572845b9f1
sharala <molayonan@gmail.com>
2025-03-31 12:59:03 +0900

    🐛 Fix: include particle folder contents in repo

---
+++ particle/test.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/**
+ * @brief main entry point
+ *
+ * Prints out "Hello, World!" string to standard output and
+ * returns 0 to the operating system.
+ *
+ * @return 0
+ */
+int32_t main(void)
+{
+    printf("Hello, World! \n");
+    
+    return 0; // return 0 to operating system
+}
